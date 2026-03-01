a26ead6e5f94d59ce18f4a49ff026aefea0e5905
Flavia Dary Joseph <flaviadaryjoseph@gmail.com>
2026-03-01 22:49:51 +0530

    Final fix - correct output format with header

--- fcfs.c
+++ fcfs.c
@@ -18,7 +18,7 @@ int main() {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
-    // Sort by arrival time
+    // Sort by arrival time (FCFS rule)
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (p[i].arrival > p[j].arrival) {
@@ -29,18 +29,19 @@ int main() {
         }
     }
 
-    int current_time = 0;
+    int completion_time = 0;
 
     for (int i = 0; i < n; i++) {
 
-        if (current_time < p[i].arrival)
-            current_time = p[i].arrival;
+        if (completion_time < p[i].arrival)
+            completion_time = p[i].arrival;
 
-        p[i].waiting = current_time - p[i].arrival;
-        current_time += p[i].burst;
+        p[i].waiting = completion_time - p[i].arrival;
+        completion_time += p[i].burst;
     }
 
-    // PRINT ONLY WAITING TIME
+    // EXACT OUTPUT FORMAT
+    printf("Waiting Time:\n");
     for (int i = 0; i < n; i++) {
         printf("%s %d\n", p[i].pid, p[i].waiting);
     }
