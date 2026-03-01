395348f1bb479cd6ef67be8cd86c94da4726db1a
Flavia Dary Joseph <flaviadaryjoseph@gmail.com>
2026-03-01 21:50:25 +0530

    Completed FCFS logic

--- src/fcfs.c
+++ src/fcfs.c
@@ -19,9 +19,47 @@ int main() {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
-    // TODO: Sort by arrival time
-    // TODO: Compute waiting & turnaround time
-    // TODO: Print EXACTLY in required format
+        
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (p[j].arrival > p[j + 1].arrival) {
+                Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+
+    int current_time = 0;
+    float total_waiting = 0, total_turnaround = 0;
+
+    
+    for (int i = 0; i < n; i++) {
+
+        if (current_time < p[i].arrival)
+            current_time = p[i].arrival;
+
+        p[i].waiting = current_time - p[i].arrival;
+        p[i].turnaround = p[i].waiting + p[i].burst;
+
+        current_time += p[i].burst;
+
+        total_waiting += p[i].waiting;
+        total_turnaround += p[i].turnaround;
+    }
+
+    
+    for (int i = 0; i < n; i++) {
+        printf("%s WT %d\n", p[i].pid, p[i].waiting);
+    }
+
+    
+    for (int i = 0; i < n; i++) {
+        printf("%s TAT %d\n", p[i].pid, p[i].turnaround);
+    }
+
+    printf("Average Waiting Time: %.2f\n", total_waiting / n);
+    printf("Average Turnaround Time: %.2f\n", total_turnaround / n);
 
     return 0;
 }
