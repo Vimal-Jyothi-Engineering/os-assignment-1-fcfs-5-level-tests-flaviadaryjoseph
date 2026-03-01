5edf1bdf291f7c26d1b644eb20607fd832c813c4
Flavia Dary Joseph <flaviadaryjoseph@gmail.com>
2026-03-01 22:47:22 +0530

    Fixed output format - print only waiting time

--- fcfs.c
+++ fcfs.c
@@ -6,7 +6,6 @@ typedef struct {
     int arrival;
     int burst;
     int waiting;
-    int turnaround;
 } Process;
 
 int main() {
@@ -19,6 +18,17 @@ int main() {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
+    // Sort by arrival time
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (p[i].arrival > p[j].arrival) {
+                Process temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+
     int current_time = 0;
 
     for (int i = 0; i < n; i++) {
@@ -27,13 +37,12 @@ int main() {
             current_time = p[i].arrival;
 
         p[i].waiting = current_time - p[i].arrival;
-        p[i].turnaround = p[i].waiting + p[i].burst;
-
         current_time += p[i].burst;
     }
 
+    // PRINT ONLY WAITING TIME
     for (int i = 0; i < n; i++) {
-        printf("%s %d %d\n", p[i].pid, p[i].waiting, p[i].turnaround);
+        printf("%s %d\n", p[i].pid, p[i].waiting);
     }
 
     return 0;
