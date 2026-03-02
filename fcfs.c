938970082f44764e7122dcf0e5e063781d9394c0
Flavia Dary Joseph <flaviadaryjoseph@gmail.com>
2026-03-02 21:39:40 +0530

    Final fix: correct waiting time logic

--- fcfs.c
+++ fcfs.c
@@ -15,52 +15,57 @@ int main() {
 
     struct Process p[n];
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
 
-    // SORT by arrival time (FCFS rule)
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = i + 1; j < n; j++) {
-            if(p[i].arrival > p[j].arrival) {
-                struct Process temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
+    // Sort by arrival time
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (p[j].arrival > p[j + 1].arrival) {
+                struct Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
         }
     }
 
     int current_time = 0;
-    float total_waiting = 0;
-    float total_turnaround = 0;
+    float total_wait = 0;
+    float total_tat = 0;
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
 
-        // If CPU is idle
-        if(current_time < p[i].arrival) {
+        // If CPU is idle, jump to arrival time
+        if (current_time < p[i].arrival) {
             current_time = p[i].arrival;
         }
 
+        // Waiting time = start time - arrival time
         p[i].waiting = current_time - p[i].arrival;
+
+        // Execute process
         current_time += p[i].burst;
+
+        // Turnaround time
         p[i].turnaround = p[i].waiting + p[i].burst;
 
-        total_waiting += p[i].waiting;
-        total_turnaround += p[i].turnaround;
+        total_wait += p[i].waiting;
+        total_tat += p[i].turnaround;
     }
 
     printf("Waiting Time:\n");
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         printf("%s %d\n", p[i].pid, p[i].waiting);
     }
 
     printf("Turnaround Time:\n");
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         printf("%s %d\n", p[i].pid, p[i].turnaround);
     }
 
-    printf("Average Waiting Time: %.2f\n", total_waiting / n);
-    printf("Average Turnaround Time: %.2f", total_turnaround / n);
+    printf("Average Waiting Time: %.2f\n", total_wait / n);
+    printf("Average Turnaround Time: %.2f", total_tat / n);
 
     return 0;
 }
