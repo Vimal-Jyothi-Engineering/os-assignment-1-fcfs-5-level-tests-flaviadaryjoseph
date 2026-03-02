99ea8b4ee30887c8f3f7d759ded3cf3fa5d2e87d
Flavia Dary Joseph <flaviadaryjoseph@gmail.com>
2026-03-02 21:24:43 +0530

    FCFS scheduling program ready for submission

--- fcfs.c
+++ fcfs.c
@@ -1,49 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct {
+struct Process {
     char pid[10];
     int arrival;
     int burst;
     int waiting;
-} Process;
+    int turnaround;
+};
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    struct Process p[100];
+    int n = 0;
 
-    Process p[n];
-
-    for (int i = 0; i < n; i++) {
-        scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
+    // Read until EOF
+    while (scanf("%s %d %d", p[n].pid, &p[n].arrival, &p[n].burst) == 3) {
+        n++;
     }
 
-    // Sort by arrival time (FCFS rule)
+    // Sort by arrival time
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (p[i].arrival > p[j].arrival) {
-                Process temp = p[i];
+                struct Process temp = p[i];
                 p[i] = p[j];
                 p[j] = temp;
             }
         }
     }
 
-    int completion_time = 0;
-
+    int current_time = 0;
     for (int i = 0; i < n; i++) {
+        if (current_time < p[i].arrival) current_time = p[i].arrival;
 
-        if (completion_time < p[i].arrival)
-            completion_time = p[i].arrival;
+        p[i].waiting = current_time - p[i].arrival;
+        p[i].turnaround = p[i].waiting + p[i].burst;
 
-        p[i].waiting = completion_time - p[i].arrival;
-        completion_time += p[i].burst;
-    }
+        current_time += p[i].burst;
 
-    // EXACT OUTPUT FORMAT
-    printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%s %d\n", p[i].pid, p[i].waiting);
+        printf("%s %d %d\n", p[i].pid, p[i].waiting, p[i].turnaround);
     }
 
     return 0;
