e118d51b6d9f023708141968088ac832251c4f76
Flavia Dary Joseph <flaviadaryjoseph@gmail.com>
2026-03-02 21:43:31 +0530

    Final corrected FCFS

--- fcfs.c
+++ fcfs.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 struct Process {
     char pid[10];
@@ -31,23 +30,15 @@ int main() {
     }
 
     int current_time = 0;
-    float total_wait = 0;
-    float total_tat = 0;
+    float total_wait = 0, total_tat = 0;
 
     for (int i = 0; i < n; i++) {
 
-        // If CPU is idle, jump to arrival time
-        if (current_time < p[i].arrival) {
+        if (current_time < p[i].arrival)
             current_time = p[i].arrival;
-        }
 
-        // Waiting time = start time - arrival time
         p[i].waiting = current_time - p[i].arrival;
-
-        // Execute process
         current_time += p[i].burst;
-
-        // Turnaround time
         p[i].turnaround = p[i].waiting + p[i].burst;
 
         total_wait += p[i].waiting;
@@ -55,14 +46,12 @@ int main() {
     }
 
     printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].waiting);
-    }
 
     printf("Turnaround Time:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].turnaround);
-    }
 
     printf("Average Waiting Time: %.2f\n", total_wait / n);
     printf("Average Turnaround Time: %.2f", total_tat / n);
